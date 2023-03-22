7d16e60d08fb5e964f582c49e8ad2b613b53e7e9
ubuntu <ubuntu@localhost.localdomain>
2023-03-22 10:37:11 +0800

    first test

---
+++ test/client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define SERV_IP "127.0.0.1"
+#define SERV_PORT 6666
+
+int main()
+{
+    int cfd;
+    struct sockaddr_in serv_addr;
+    char buf[BUFSIZ];
+    int n;
+
+    cfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // memset(&serv_addr, 0, sizeof(serv_addr));
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);
+
+    connect(cfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    while(1) {
+        fgets(buf, sizeof(buf), stdin);  // "hello world" --> fgets --> "hello world\n\0"
+        write(cfd, buf, strlen(buf));
+        n =read(cfd, buf, sizeof(buf));
+        write(STDOUT_FILENO, buf, n);
+    }
+
+    close(cfd);
+
+    return 0;
+}
---
+++ test/server.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <string.h>
+
+#define SERV_PORT 6666
+#define SERV_IP "127.0.0.1"
+
+int main() {
+    int lfd, cfd;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t cli_addr_len;
+    char buf[BUFSIZ];
+    int n;
+
+    lfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // memset(&serv_addr, 0, sizeof(serv_addr));
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);
+    
+    bind(lfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    listen(lfd, 128);
+    
+    cli_addr_len = sizeof(cli_addr);
+    cfd = accept(lfd, (struct sockaddr*)&cli_addr, &cli_addr_len);
+    while (1) {
+        n = read(cfd, buf, sizeof(buf));
+        for(int i = 0; i < n; i++)
+        {
+        buf[i] = toupper(buf[i]);
+        }
+        write(cfd, buf, n);  
+    }
+
+    close(lfd);
+    close(cfd);
+
+    return 0;
+}
