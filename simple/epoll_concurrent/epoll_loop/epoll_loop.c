ba658c48ad4ccd739c24022c81a23a8f65988020
ubuntu <1565798971@qq.com>
2023-03-25 00:10:18 +0800

    threadpool

--- simple/epoll_concurrent/epoll_loop/epoll_loop.c
+++ simple/epoll_concurrent/epoll_loop/epoll_loop.c
@@ -34,8 +34,8 @@ struct myevent_s {
     long last_active;                                       //记录每次加入红黑树 g_efd 的时间值
 };
 
-int g_efd;                                                  //全局变量, 保存epoll_create返回的文件描述符
-struct myevent_s g_events[MAX_EVENTS+1];                    //自定义结构体类型数组. +1-->listen fd
+int g_efd;                                                  // 红黑树树根，全局变量, 保存epoll_create返回的文件描述符
+struct myevent_s g_events[MAX_EVENTS+1];                    // 描述要监听的文件描述符，自定义结构体类型数组. +1-->listen fd
 
 
 /*将结构体 myevent_s 成员变量 初始化*/
@@ -234,17 +234,16 @@ int main(int argc, char *argv[])
 
     initlistensocket(g_efd, port);                      //初始化监听socket
 
-    struct epoll_event events[MAX_EVENTS+1];            //保存已经满足就绪事件的文件描述符数组 
+    struct epoll_event events[MAX_EVENTS+1];            // 保存已经满足就绪事件的文件描述符数组，epoll_wait传出参数
 	printf("server running:port[%d]\n", port);
 
     int checkpos = 0, i;
     while (1) {
         /* 超时验证，每次测试100个链接，不测试listenfd 当客户端60秒内没有和服务器通信，则关闭此客户端链接 */
-
         long now = time(NULL);                          //当前时间
         for (i = 0; i < 100; i++, checkpos++) {         //一次循环检测100个。 使用checkpos控制检测对象
             if (checkpos == MAX_EVENTS)
-                checkpos = 0;
+                checkpos = 0; 
             if (g_events[checkpos].status != 1)         //不在红黑树 g_efd 上
                 continue;
 
@@ -260,7 +259,7 @@ int main(int argc, char *argv[])
         /*监听红黑树g_efd, 将满足的事件的文件描述符加至events数组中, 1秒没有事件满足, 返回 0*/
         int nfd = epoll_wait(g_efd, events, MAX_EVENTS+1, 1000);
         if (nfd < 0) {
-            printf("epoll_wait error, exit\n");
+            printf("epoll_wait error, exit\n"); 
             break;
         }
 
